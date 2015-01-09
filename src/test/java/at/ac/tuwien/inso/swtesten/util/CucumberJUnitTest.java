62622e14cbad37481b3736eb36ba9f2fb5869e04
Matthias Karan <matthias.karan@gmail.com>
2015-01-09 14:41:31 +0100

    uncomment

--- src/test/java/at/ac/tuwien/inso/swtesten/util/CucumberJUnitTest.java
+++ src/test/java/at/ac/tuwien/inso/swtesten/util/CucumberJUnitTest.java
@@ -16,16 +16,16 @@ import cucumber.api.junit.Cucumber;
  * when developing your own Cucumber test cases in the lab.
  */
 
-@RunWith(Cucumber.class)
+/*@RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/sample"}, 
 				 glue = {"at.ac.tuwien.inso.swtesten.sample"}, 
 				 format = {"html:target/cucumber-html-report","json:target/cucumber-json-report.json"}
-)
+)*/
 
-//@CucumberOptions(features={"src/test/resources/lab"}, 
-//				 glue= {"at.ac.tuwien.inso.swtesten.lab"}, 
-//				 format = {"html:target/cucumber-html-report","json:target/cucumber-json-report.json"}
-//)
+@CucumberOptions(features={"src/test/resources/lab"}, 
+				 glue= {"at.ac.tuwien.inso.swtesten.lab"}, 
+				 format = {"html:target/cucumber-html-report","json:target/cucumber-json-report.json"}
+)
 
 public class CucumberJUnitTest {
 }
